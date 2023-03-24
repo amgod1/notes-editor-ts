e885399b80488089fd331a9557478542c711bf63
Schepuk Kamill <kamillschepuk@gmail.com>
2023-03-24 17:59:43 +0300

    refactor: ru tags added

--- src/helpers/findTags.ts
+++ src/helpers/findTags.ts
@@ -1,7 +1,7 @@
 import { Note, Tag } from '../reducer/types';
 
 export const findTagsString = (str: string): Tag[] => {
-	const matches = str.match(/#\w+/g);
+	const matches = str.match(/#[\p{L}\d]+/gu);
 	return matches
 		? matches
 				.filter((tag): tag is string => tag != null)
@@ -11,7 +11,7 @@ export const findTagsString = (str: string): Tag[] => {
 
 export const findTagsArray = (notes: Note[]): Tag[] =>
 	notes
-		.map(({ content }) => content.match(/#\w+/g))
+		.map(({ content }) => content.match(/#[\p{L}\d]+/gu))
 		.flat()
 		.filter((match): match is string => match != null)
 		.map(tag => ({ tag, id: crypto.randomUUID(), checked: false }));
